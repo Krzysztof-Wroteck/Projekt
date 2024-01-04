18446aca26b68442020de56c0b488ea7dcf983ae
Krzysztof <krzysztof.wrotecki753@gmail.com>
2024-01-04 13:22:14 +0100

    Poczatek Projektu

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
+import '@fortawesome/fontawesome-free/css/solid.css';
+
+window.$ = window.jQuery = require('jquery');
